2b444130ce08166201af7c73b676e0420d0721d9
Dima <57713733+Dima1409@users.noreply.github.com>
2023-02-05 20:06:24 +0200

    fix getAll

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,23 +1,51 @@
 const { Contact } = require("../../models");
 
+const getAllContacts = async (userId, query) => {
+  const { page = 1, limit = 20, favorite, name } = query;
+  const skip = (page - 1) * limit;
+  const pagination = {
+    skip,
+    limit: Number(limit),
+  };
+  const ownerInfo = "_id email";
+  if (favorite && !name) {
+    return Contact.find({ owner: userId, favorite: favorite }, "", pagination)
+      .populate("owner", ownerInfo)
+      .sort({ name: 1 });
+  }
+
+  if (name && !favorite) {
+    return Contact.find(
+      { owner: userId, name: { $regex: name } },
+      "",
+      pagination
+    )
+      .populate("owner", ownerInfo)
+      .sort({ name: 1 });
+  }
+
+  if (favorite && name) {
+    return Contact.find(
+      { owner: userId, favorite: favorite, name: { $regex: name } },
+      "",
+      pagination
+    )
+      .populate("owner", ownerInfo)
+      .sort({ name: 1 });
+  }
+
+  return Contact.find({ owner: userId }, "", pagination)
+    .populate("owner", ownerInfo)
+    .sort({ name: 1 });
+};
+
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20, favorite } = req.query;
-  const skip = (page - 1) * limit;
-  const contacts = await Contact.find(
-    favorite ? { owner: _id, favorite: favorite } : { owner: _id },
-    "",
-    {
-      skip,
-      limit: Number(limit),
-    }
-  )
-    .populate("owner", "_id name email")
-    .sort({ name: 1 });
+  const result = await getAllContacts(_id, req.query);
   res.status(200).json({
     status: "success",
     data: {
-      contacts,
+      result,
     },
   });
 };
