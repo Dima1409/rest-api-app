9b23125434338eabe9c486d94298e88421aa73b9
Dima <57713733+Dima1409@users.noreply.github.com>
2023-02-05 22:36:07 +0200

    fix filter

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,13 +1,18 @@
 const { Contact } = require("../../models");
 
 const getAllContacts = async (userId, query) => {
-  const { page = 1, limit = 20, favorite = false, name = "" } = query;
+  const { page = 1, limit = 20, favorite, name } = query;
   const skip = (page - 1) * limit;
   const pagination = {
     skip,
     limit: Number(limit),
   };
-  const filter = { owner: userId, favorite: favorite, name: { $regex: name } };
+
+  const filter = {};
+  if (userId) filter.owner = userId;
+  if (favorite) filter.favorite = favorite;
+  if (name) filter.name = { $regex: name };
+
   const ownerInfo = "_id email";
   return Contact.find(filter, "", pagination)
     .populate("owner", ownerInfo)
